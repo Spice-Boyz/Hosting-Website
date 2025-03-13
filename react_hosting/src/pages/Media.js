feab6cd97e59cbc0662e0bbf036d1775d1cecc2f
Subomiadekunle <subomiade25@gmail.com>
2025-03-13 10:15:40 -0500

    removed unscessary files that made it messy

---
+++ react_hosting/src/pages/Media.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Media extends React.Component{
+    render(){
+        return(
+        <main>
+        <div classname ="">
+        <div style={{ height: "100vh" }}></div>
+        <h1>Media Gallery</h1>
+            <p>Photos and videos from past events will be showcased here.</p>
+
+        </div>
+    </main>)
+    }
+}
+export default Media; 
