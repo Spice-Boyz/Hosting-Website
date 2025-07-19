36db5e13cb92a36e682e83d1f845ee21e398f853
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-07-19 16:46:51 -0500

    unsuccseful instagram implementation

--- spiceboyz/src/pages/Main.jsx
+++ spiceboyz/src/pages/Main.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
 import PlaylistScroller from '../components/PlaylistsScroller'; // go up one level, then into components
 import '../components/PlaylistScroller.css'; // CSS file in the same folder
 
+
 function Main() {
+
     return (
         
         <main   >
-            <div style={{height: "10vh"}}></div>
+            <div style={{height: "20vh"}}></div>
            
              <div  className="announcement-container" data-aos="fade-up"> 
                 <h2>SPICEBOYZ TURNS 1 !</h2>
                 <h3>#SB4L</h3>
+                 <iframe
+                src="https://www.instagram.com/p/DIW-yZSSK4h/?img_index=9"
+                width="328"
+                height="520"
+               
+                
+                allowTransparency={true}
+            ></iframe>
+                
                 <p style={{height: "10vh"}}></p>
                 <h4>Tickets to next party:</h4>
                 <p>Eventbrite API</p>
+            
+           
             </div>
+
 
             <div className = "playlists" > 
              <h2 >Editors Picks:</h2>
