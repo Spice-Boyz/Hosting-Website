736de9aa8246668f9f99b8cbfc73b2b530ac686f
Subomiadekunle <subomiade25@gmail.com>
2025-07-21 08:49:47 -0500

    ayo playlist

---
+++ .history/spiceboyz/src/pages/About_20250714235141.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section>
+        
+        <div style={{backgroundColor: 'black'}}></div>
+        <div className = "all-four">
+
+        <ul>
+            <li>
+                <img src="/assets/SPICEBOYZCHROME.png"  className="small-image"  width="450" height="450"/></li>
+            
+          </ul> 
+          
+        </div>
+
+         <div className ="aboutus">
+            <h1>MISSION STATEMENT !</h1>
+            <p>Cultivate an environment suitable for everyone of all different walks </p>
+            <p> of life, To feel free, bringing a breath of fresh air to the young saint louis party scene</p>
+            <p>I forgot our mission statement</p>
+            <h3>#SB4L</h3>
+        </div>
+        <div style={{height: "25vh"}}></div>
+    </section>
+
+    );
+}
+
+export default About;
