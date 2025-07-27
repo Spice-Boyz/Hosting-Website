144a7ca62cdc8396da9f00a1578918ec2b01a31b
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-07-26 22:41:26 -0500

    turned everything jsx

--- spiceboyz/src/App.js
+++ spiceboyz/src/App.js
@@ -4,9 +4,6 @@ import Loading from './components/Loading';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-import { useEffect } from 'react';
-
-
 
 import Header from './components/Header';
 import Footer from './components/Footer';
