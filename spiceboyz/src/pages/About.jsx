c86f6f8b7ee6c755b138213c563e61138bf89c40
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-07-29 15:57:35 -0500

    bruh

--- spiceboyz/src/pages/About.jsx
+++ spiceboyz/src/pages/About.jsx
@@ -16,7 +16,7 @@ function About() {
         </div>
 
          <div className ="aboutus" data-aos="fade-up">
-            <h1>MISSION STATEMENT !</h1>
+            <h3>MISSION !</h3>
             <h4>At Spiceboyz, we are more than just a collective group; we are purveyors of fun and joy. Our mission is simple yet profound: to spread good vibes and purity wherever we go. Join us on our journey as we bring people together, one beat at a time, with an unwavering dedication to spreading happiness and pure joy.</h4>
             <h3>#SB4L</h3>
         </div>
