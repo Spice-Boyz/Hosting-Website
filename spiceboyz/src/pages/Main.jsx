f0e6ee08c7b3d236eea6b44d6eeffb0ea4b979df
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-08-22 10:49:41 -0500

    countdown

--- spiceboyz/src/pages/Main.jsx
+++ spiceboyz/src/pages/Main.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import PlaylistScroller from '../components/PlaylistsScroller'; // go up one level, then into components
 import '../components/PlaylistScroller.css'; // CSS file in the same folder
 import '../components/EventBrite'; 
@@ -5,11 +6,48 @@ import ReviewWidget from '../components/ReviewWidget';
 import EventBrite from '../components/EventBrite';
 
 function Main() {
+    const tickRef = useRef(null);
+
+  useEffect(() => {
+    if (!document.getElementById("tickcounter-sdk")) {
+      const s = document.createElement("script");
+      s.id = "tickcounter-sdk";
+      s.src = "https://www.tickcounter.com/static/js/loader.js";
+      document.body.appendChild(s);
+    } else {
+      if (window.tc_widgetloader && typeof window.tc_widgetloader.load === "function") {
+        window.tc_widgetloader.load();
+      }
+    }
+  }, []);
 
     return (
         
         <main   >
-            <div style={{height: "35vh"}}></div>
+            <div style={{height: "25vh"}}></div>
+
+            <div style ={{}} className = "Countdown">
+                <a
+                    ref={tickRef}
+                    data-type="countdown"
+                    data-name="LASGIDI PREP BEGINS IN:"
+                    data-bg_color="#000000ff"
+                    data-name_color="#f6682f"
+                    data-border_color="#fafafaff"
+                    data-digits_color = "#f8efefff"
+                    data-last_unit_color = "#f6682f"
+                    data-font = "Courier New"
+                    data-dt="2025-08-29 22:00"
+                    data-timezone="America/Chicago"
+                    className="tickcounter"
+                    href="https://www.tickcounter.com"
+                    style={{ display: "block", width: "45%", position: "relative", paddingBottom: "25%" , textAlign: "center", color: "white", backgroundColor: "black" , marginLeft: "27%"}}
+                >
+                    Countdown
+                </a>
+            </div>
+
+            <div style={{height: "15vh"}}></div>
            
              <div  className="announcement-container" data-aos="fade-up"> 
                 <div style={{height: "15vh"}}></div>
